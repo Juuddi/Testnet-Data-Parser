7e6543541520f0c352e1154d9b8b2a045b20153b
Justin Rudi <justinrudi@Justins-MacBook-Pro.local>
2022-04-15 02:32:50 -0500

    Fixed minor comment errors and cleaned up the code structure.

--- dataparser.py
+++ dataparser.py
@@ -1,17 +1,11 @@
-from turtle import addshape
 import pandas as pd
 
 #minedBlocks = pd.read_csv(r"AggregateBlkData.csv", usecols = ['Prime', 'Region1', 'Region2', 'Region3', 'Zone11', 'Zone12', 'Zone13', 'Zone21', 'Zone22', 'Zone23', 'Zone31', 'Zone32', 'Zone33'])
 #txs = pd.read_csv(r"AggregateTxData.csv", usecols = ['Prime', 'Region1', 'Region2', 'Region3', 'Zone11', 'Zone12', 'Zone13', 'Zone21', 'Zone22', 'Zone23', 'Zone31', 'Zone32', 'Zone33'])
 df = pd.read_csv(r"Addresses.csv", usecols = ["Total", "Discord User Names"])
 
-# Notes for tomorrow: compile addresses spreadsheet into 2 columns - split addresses from 1 single column
 
-
-#Use functions var.split and var.lower to lowercase and split all addresses into lists
-
-
-#Create dictionary with key: discord name and value: addresses 
+#Creates dictionary with key: discord name and value: addresses 
 #Dictionary has been verified against original dataset!!
 dicts = {}
 
