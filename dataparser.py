48e7086a9570fe5ee6111b54411fbda4c8fbca4b
Justin Rudi <justinrudi@Justins-MacBook-Pro.local>
2022-04-25 18:19:24 -0500

    Final testing and cleaning up script

--- dataparser.py
+++ dataparser.py
@@ -1,5 +1,3 @@
-from operator import index
-from numpy import number
 import pandas as pd
 
 #Import .csvs
@@ -13,7 +11,34 @@ nodes = pd.read_csv(r"NodeData.csv", usecols = ["Discord User Names", "Number of
 minedBlocks = minedBlocks.applymap(lambda s: s.lower() if type(s) == str else s)
 txs = txs.applymap(lambda s: s.lower() if type(s) == str else s)
 
-# NodeData.csv still needs to be created with the format shown in the algorithm below
+#List of values to be removed from address responses
+remove_values = ["0x0767d31b0d7671c3e97c6abed055a26fb59b4149", "0x11a03db52d12201e614466cb98ec5d49a1205bda", "0x3bcec1847c55246cf9ea32a5dfe652f147ac091c", "0x5e6b0261c32b25f187786612d27a39f6d0c31771", "0x1a6ad97c8f06c7ae79fea47e43a8c048da5b1f7d", "0x186da447ec1dd29cdec8cca5653ccc4fd8f9e5e3", "0x2ab56840530b1c395ecf91e5923446fa696c7933", "0x454f47e9da39a4d2cff17d7ca50757576a298fb2", "0x4c190ab6136e94b3b510172784a4fed22f566622", "0x5446d13d4907630425928fceb67ca35a6bf1bb0e", "0x677c5623aabeb5d6d978cc2ec11ac5297a8afcbd", "0x7717ddddd08eacc0bb981c47348d1ec3a99566f8", "0x8169c0a78e20ee6e5c53cc18ee2f4eb3f762ee05", "", "na", "n/a", "-", "no", "nan"]
+
+#Create miner address/discord user name dictionary, split by commas and make lowercase
+mineraddresses = {}
+for x, row in mineradds.iterrows():
+    miner = str(row["Discord User Names"])
+    adds = str(row["Total"])
+    adds = adds.lower()
+    adds = adds.split(", ")
+    if adds == "nan":
+        continue
+    for x in remove_values:
+        valueToBeRemoved = x
+        adds = list(filter((x).__ne__,adds))
+    mineraddresses[miner] = adds
+
+#Create citizen address/discord username dictionary, split by commas and make lowercase
+citizenaddresses = {}
+for x, row in citizenadds.iterrows():
+    citizen = str(row["Discord User Names"])
+    adds = str(row["Wallet Addresses"])
+    if adds == "nan":
+        continue
+    adds = adds.lower()
+    adds = adds.split(", ")
+    citizenaddresses[citizen] = adds
+
 # Create node-reward/discord username dictionary
 noderewards = {}
 for x, row in nodes.iterrows():
@@ -36,70 +61,36 @@ for x, row in nodes.iterrows():
     reward = multiplier*numberOfNodes + base
     noderewards[operator] = reward
 
-
-#List of values to be removed from address responses
-remove_values = ["0x0767d31b0d7671c3e97c6abed055a26fb59b4149", "0x11a03db52d12201e614466cb98ec5d49a1205bda", "0x3bcec1847c55246cf9ea32a5dfe652f147ac091c", "0x5e6b0261c32b25f187786612d27a39f6d0c31771", "0x1a6ad97c8f06c7ae79fea47e43a8c048da5b1f7d", "0x186da447ec1dd29cdec8cca5653ccc4fd8f9e5e3", "0x2ab56840530b1c395ecf91e5923446fa696c7933", "0x454f47e9da39a4d2cff17d7ca50757576a298fb2", "0x4c190ab6136e94b3b510172784a4fed22f566622", "0x5446d13d4907630425928fceb67ca35a6bf1bb0e", "0x677c5623aabeb5d6d978cc2ec11ac5297a8afcbd", "0x7717ddddd08eacc0bb981c47348d1ec3a99566f8", "0x8169c0a78e20ee6e5c53cc18ee2f4eb3f762ee05", "", "na", "n/a", "-", "no", "nan"]
-
-#Create miner address/discord user name dictionary, split by commas and make lowercase
-mineraddresses = {}
-for x, row in mineradds.iterrows():
-    miner = str(row["Discord User Names"])
-    adds = str(row["Total"])
-    adds = adds.lower()
-    adds = adds.split(", ")
-    for x in remove_values:
-        valueToBeRemoved = x
-        adds = list(filter((x).__ne__,adds))
-    mineraddresses[miner] = adds
-print(mineraddresses)
-
-df = pd.DataFrame.from_dict(mineraddresses, orient="index")
-df.to_csv("Blank.csv")
-
-#Create citizen address/discord username dictionary, split by commas and make lowercase
-citizenaddresses = {}
-for x, row in citizenadds.iterrows():
-    citizen = str(row["Discord User Names"])
-    adds = str(row["Wallet Addresses"])
-    adds = adds.lower()
-    adds = adds.split(", ")
-    citizenaddresses[citizen] = adds
-
 #Compare mineraddresses against minedBlocks to generate rewards dictionary
 minerrewards = {}
 for k, v in mineraddresses.items():
     miner = k
     addresses = v
-    if v == "nan":
-        continue
     reward = 0
     for i in addresses:
-        if i == "nan":
-            continue
         for col in minedBlocks.columns:
              filtereddf = minedBlocks.loc[minedBlocks[col] == i]
              idx = len(filtereddf)
              reward = reward + idx
         minerrewards[miner] = reward
 
-df = pd.DataFrame.from_dict(minerrewards, orient="index")
-df.to_csv("Blank.csv")
-
 #Compare citizenaddresses against txs to generate citizen rewards
 citizenrewards = {}
 for k, v in citizenaddresses.items():
-    k = citizen
-    v = addresses
-    if v == "nan":
-        continue
-    rewards = 0
+    citizen = k
+    addresses = v
+    reward = 0
     for i in addresses:
-        if i == "nan":
-            continue
         for col in txs.columns:
             filtereddf = txs.loc[txs[col] == i]
             idx = len(filtereddf)
             reward = reward + idx
         citizenrewards[citizen] = reward
 
-#Last thing that needs to be done - combine all rewards dictionaries in some fashion and write them all to a .csv file
+#Output rewards dictionaries to .csv files
+df = pd.DataFrame.from_dict(noderewards, orient="index")
+df.to_csv("NodeRewards.csv")
+df = pd.DataFrame.from_dict(minerrewards, orient="index")
+df.to_csv("MinerRewards.csv")
+df = pd.DataFrame.from_dict(citizenrewards, orient="index")
+df.to_csv("CitizenRewards.csv")
